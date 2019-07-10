9da7361077988cf3a79615bec4ff10ac24ccc22b
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-07-10 05:31:26 -0300

    nova tentativa

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
+## ----------------------------------------------------------
+## Importação dos módulos padrões
+## ----------------------------------------------------------
 import os, pymongo
 from flask import Flask, jsonify, request, render_template
 from flask_json_schema import JsonSchema, JsonValidationError
 
-from mod.pessoa import pessoa_blue
+## ----------------------------------------------------------
+## Importação dos módulos dos Blueprints
+## ----------------------------------------------------------
+from blueprints_list.pessoa import pessoa_blue
 
+## ----------------------------------------------------------
+## Instanciando a API
+## ----------------------------------------------------------
 app = Flask(__name__)
-schema = JsonSchema(app)
-
 app.config.from_object('settings')
+
+## ----------------------------------------------------------
+## Lista de Blueprints
+## ----------------------------------------------------------
 app.register_blueprint(pessoa_blue, url_prefix="/pessoa")
 
 ##app.config['MONGO_DBNAME'] = 'TCC.Pessoas'
@@ -16,29 +27,37 @@ app.register_blueprint(pessoa_blue, url_prefix="/pessoa")
 
 
 ## ----------------------------------------------------------
-## Rotas padrões
+## Tratamento de erros na validação
 ## ----------------------------------------------------------
-
-## Setando tratamento de erros na validação
 @app.errorhandler(JsonValidationError)
 def validation_error(e):
     return jsonify({ 'Erro': e.message, 'Errors': [validation_error.message for validation_error in e.errors]})
 
+
+## ----------------------------------------------------------
+## Rotas padrões
+## ----------------------------------------------------------
+##
+## @pessoa_blue.route: A rota do endpoint
+## ----------------------------------------------------------
+
+## ----------------------------------------------------------
 ## Endpoint de boas vindas
+## ----------------------------------------------------------
 @app.route("/hi")
 def boas_vindas():
     return render_template("bem_vindos.html")
 
+## ----------------------------------------------------------
 ## Endpoint index
+## ----------------------------------------------------------
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 ## ----------------------------------------------------------
 ## configuração de IP e porta
 ## ----------------------------------------------------------
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)

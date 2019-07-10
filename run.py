03e87b009066a59f3325e01d003423939662f619
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-07-10 03:34:30 -0300

    primeira versão escalável

--- run.py
+++ run.py
@@ -1,2 +1,6 @@
 from create_app import app
 app.run(debug=False, use_reloader=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

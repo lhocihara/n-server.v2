88b41c2793b5dbfb7a666660a288a79a5506b5a6
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-07-10 05:46:21 -0300

    nova tentativa

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from flask_json_schema import JsonSchema, JsonValidationError
 ## ----------------------------------------------------------
 ## Importação dos módulos dos Blueprints
 ## ----------------------------------------------------------
-from blueprints.pessoa import pessoa_blue
+from endpoints.pessoa import pessoa_blue
 
 ## ----------------------------------------------------------
 ## Instanciando a API

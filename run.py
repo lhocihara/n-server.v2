31c7863cc3339e1b5ed5fb4a3550dd39f57e015c
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-07-10 21:29:54 -0300

    atualização

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 from main import app
 import os
-app.run(debug=False, use_reloader=True)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+app.run(debug=False, use_reloader=True)

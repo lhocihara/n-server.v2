9b5a883f3fb6032ad3ae77720cf5862dcdb1705a
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-07-10 21:32:18 -0300

    atualização

--- run.py
+++ run.py
@@ -2,3 +2,10 @@ from main import app
 import os
 
 app.run(debug=False, use_reloader=True)
+
+## ----------------------------------------------------------
+## configuração de IP e porta
+## ----------------------------------------------------------
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

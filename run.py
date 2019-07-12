98b80dd86375715bfd1987f3b22a537188c5b1a5
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-07-12 12:53:02 -0300

    atualizando api e metodo de cadastar pessoa

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ envi_prod = True
 ## ----------------------------------------------------------
 if __name__ == '__main__' and envi_prod:
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, use_reloader=False,host='0.0.0.0', port=port)
+    app.run(debug=False, use_reloader=False,host='0.0.0.0', port=port)
 
 if __name__ == '__main__' and not envi_prod:
     app.run(host='127.0.0.1', debug=False, use_reloader=True)

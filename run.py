12b94daea91f6cb9acb0b29bf8573399da9cade9
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-07-12 13:01:21 -0300

    modificando msensagem de console

--- run.py
+++ run.py
@@ -9,5 +9,5 @@ if __name__ == '__main__' and envi_prod:
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, use_reloader=False,host='0.0.0.0', port=port)
 
-if __name__ == '__main__' and not envi_prod:
+elif __name__ == '__main__' and not envi_prod:
     app.run(host='127.0.0.1', debug=False, use_reloader=True)

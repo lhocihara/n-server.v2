42dbe963d9ce9b858aba8c18ef3294cb65d670bc
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-07-12 13:35:55 -0300

    test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import requests, json
+
+headers = {'content-type': 'application/json; charset=utf-8'}
+data = {
+    "nome_completo": "Ragnar Lothbrok",
+    "cpf": "39008867890",
+    "data_nasc": "1997-10-27T00:00:00.000Z",
+    "genero": "D",
+    "email": "Sif@gmail.com",
+    "senha": "75f7313c20144e39edcf57a14733d074aee0c482320d5178ee0ef2f2608c2996"
+}
+for i in range(0,10):
+    
+    r = requests.post("http://n-server-v2.herokuapp.com/pessoa", data=json.dumps(data),headers=headers)
+    print (r.text)

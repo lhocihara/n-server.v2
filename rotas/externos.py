15b9e1457d0587e7ab0d4afe3bbe71b0cd2d82f1
Marcia Apra <marciaapra@hotmail.com>
2019-09-15 10:08:43 -0300

    Adicionar CORS API externos

--- rotas/externos.py
+++ rotas/externos.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-## ----------------------------------------------------------
-## Importação dos módulos padrões
-## ----------------------------------------------------------
+# ----------------------------------------------------------
+# Importação dos módulos padrões
+# ----------------------------------------------------------
 from flask_json_schema import JsonSchema, JsonValidationError
 from flask import Flask, Blueprint, request, jsonify
+from flask_cors import CORS, cross_origin
 from bson.objectid import ObjectId
 from datetime import datetime, timedelta
 
@@ -11,62 +12,61 @@ from datetime import datetime, timedelta
 import pymongo
 import dns
 
-## ----------------------------------------------------------
-## Importação do orquestrador da conexão com BD
-## ----------------------------------------------------------
-from orquestrador.orquestrador import Orquestrador 
-## ----------------------------------------------------------
-## Importação dos Objetos de tratamento de erros
-## ----------------------------------------------------------
+# ----------------------------------------------------------
+# Importação do orquestrador da conexão com BD
+# ----------------------------------------------------------
+from orquestrador.orquestrador import Orquestrador
+# ----------------------------------------------------------
+# Importação dos Objetos de tratamento de erros
+# ----------------------------------------------------------
 from biblioteca_respostas.status_internos import StatusInternos
 from biblioteca_respostas.respostas_api import RespostasAPI
-## ----------------------------------------------------------
-## Importação dos schemas referentes a Externos
-## ----------------------------------------------------------
+# ----------------------------------------------------------
+# Importação dos schemas referentes a Externos
+# ----------------------------------------------------------
 
 orq = Orquestrador()
 
-## ----------------------------------------------------------
-## Definição do Blueprint
-## ----------------------------------------------------------
-blueprint_externos = Blueprint("Externos",__name__)
+# ----------------------------------------------------------
+# Definição do Blueprint
+# ----------------------------------------------------------
+blueprint_externos = Blueprint("Externos", __name__)
 
-## ----------------------------------------------------------
-## Definição do Subapp e Schema
-## ----------------------------------------------------------
+# ----------------------------------------------------------
+# Definição do Subapp e Schema
+# ----------------------------------------------------------
 app = Flask("Externos")
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 schema = JsonSchema(app)
 
-## ----------------------------------------------------------
-## Rotas dos serviços para o APP
-## ----------------------------------------------------------
+# ----------------------------------------------------------
+# Rotas dos serviços para o APP
+# ----------------------------------------------------------
 ##
-## @pessoa_blue.route: A rota do endpoint
-## @schema.validate: O schema a ser validado durante a requisição
-## ----------------------------------------------------------
+# @pessoa_blue.route: A rota do endpoint
+# @schema.validate: O schema a ser validado durante a requisição
+# ----------------------------------------------------------
 
 
 @blueprint_externos.route("/gera_token", methods=['POST'])
+@cross_origin()
 def Gerar_Token():
-      try:  
-            token_request = request.json  
-            segredo = token_request['segredo']
-            retorno = orq.verificar_id_projeto_externos(segredo)
-            if retorno :
-                id_projeto = token_request['segredo']
-                token = orq.gera_hash(id_projeto)
-                vencimento = datetime.now() + timedelta(minutes=5)      
-                orq.armazenar_tokens(id_projeto, token, vencimento)
-                return RespostasAPI('Consulta realizada com sucesso',
-                                    {
-                                        'token': str(token),                                        
-                                    }
-                                    ).JSON 
-            else:
-                raise StatusInternos('SI-21', {'projeto': projeto})
-      except StatusInternos as e:
-            return e.errors
-
-
-
-
+    try:
+        token_request = request.json
+        segredo = token_request['segredo']
+        retorno = orq.verificar_id_projeto_externos(segredo)
+        if retorno:
+            id_projeto = token_request['segredo']
+            token = orq.gera_hash(id_projeto)
+            vencimento = datetime.now() + timedelta(minutes=5)
+            orq.armazenar_tokens(id_projeto, token, vencimento)
+            return RespostasAPI('Consulta realizada com sucesso',
+                                {
+                                    'token': str(token),
+                                }
+                                ).JSON
+        else:
+            raise StatusInternos('SI-21', {'projeto': segredo})
+    except StatusInternos as e:
+        return e.errors

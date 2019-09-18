28535b84f5ad28c68518404ff0d386e38ab7de42
andreymxc <54007692+andreymxc@users.noreply.github.com>
2019-09-18 16:49:33 -0300

    update gera token, validar token
    update APIs gera token, valida token

--- rotas/externos.py
+++ rotas/externos.py
@@ -4,10 +4,11 @@
 ## ----------------------------------------------------------
 from flask_json_schema import JsonSchema, JsonValidationError
 from flask import Flask, Blueprint, request, jsonify
+from flask_cors import CORS, cross_origin
 from bson.objectid import ObjectId
 from datetime import datetime, timedelta
 
-
+import hashlib
 import pymongo
 import dns
 
@@ -35,6 +36,8 @@ blueprint_externos = Blueprint("Externos",__name__)
 ## Definição do Subapp e Schema
 ## ----------------------------------------------------------
 app = Flask("Externos")
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 schema = JsonSchema(app)
 
 ## ----------------------------------------------------------
@@ -45,27 +48,48 @@ schema = JsonSchema(app)
 ## @schema.validate: O schema a ser validado durante a requisição
 ## ----------------------------------------------------------
 
-
 @blueprint_externos.route("/gera_token", methods=['POST'])
+@cross_origin()
 def Gerar_Token():
-      try:  
-            token_request = request.json  
-            segredo = token_request['segredo']
-            retorno = orq.verificar_id_projeto_externos(segredo)
-            if retorno :
-                id_projeto = token_request['segredo']
-                token = orq.gera_hash(id_projeto)
-                vencimento = datetime.now() + timedelta(minutes=5)      
-                orq.armazenar_tokens(id_projeto, token, vencimento)
-                return RespostasAPI('Consulta realizada com sucesso',
-                                    {
-                                        'token': str(token),                                        
+    try:       
+        segredo = request.json['segredo']
+        projeto_existente = orq.verificar_id_projeto_externos(segredo)
+        if projeto_existente:
+            token = hashlib.sha256((str(segredo) + str(datetime.now())).encode()).hexdigest()
+            vencimento = datetime.now() + timedelta(minutes=5)
+            orq.armazenar_tokens(segredo, token, vencimento)
+            return RespostasAPI('Token gerado com sucesso',
+                                {
+                                    'token': str(token),
+                                }
+                                ).JSON
+        else:
+            raise StatusInternos('SI-21', {'projeto': segredo})
+    except StatusInternos as e:
+        return e.errors
+      
+
+@blueprint_externos.route("/valida_token", methods=['POST'])
+@cross_origin()
+def Validar_Token():
+    try:        
+        token = request.json['token']
+        info_token = orq.consulta_info_token(token)
+        vencimento_token = info_token['vencimento']
+        projeto_token = info_token['id_projeto']       
+        if datetime.now() < vencimento_token:
+            projeto = orq.verificar_id_projeto(projeto_token)
+            json_retorno = RespostasAPI('Token válido',
+                                    { "token" : token,
+                                      "id_projeto" : projeto_token,
+                                      "objeto_projeto" : projeto  
                                     }
-                                    ).JSON 
-            else:
-                raise StatusInternos('SI-21', {'projeto': projeto})
-      except StatusInternos as e:
-            return e.errors
+                                    ).JSON
+            return json_retorno      
+        else:
+            raise StatusInternos('SI-22', {'projeto': projeto_token})
+    except StatusInternos as e:
+        return e.errors
 
 
 
